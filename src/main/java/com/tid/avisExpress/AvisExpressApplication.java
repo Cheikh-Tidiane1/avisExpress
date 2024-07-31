8a12afc080058477a4738efa8296c40bec5daad6
cheikh tidiane <cheikh951812@gmail.com>
2024-07-31 23:56:07 +0200

    first commit

---
+++ src/main/java/com/tid/avisExpress/AvisExpressApplication.java
@@ -0,0 +1,13 @@
+package com.tid.avisExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tid/avisExpress/AvisExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tid.avisExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
